a4bcd5f3a6d2e3ce0cda5328f671575547e77404
Vicky <107568580+Vignesh-72@users.noreply.github.com>
2023-08-16 17:12:52 +0530

    Update assistx.rs

--- src/assistx.rs
+++ src/assistx.rs
@@ -3,7 +3,6 @@ use regex::Regex;
 use serde::Deserialize;
 use dotenv::dotenv;
 
-
 #[derive(Deserialize,Debug)]
 struct HuggingFaceResponse{
   generated_text : String,
@@ -11,8 +10,8 @@ struct HuggingFaceResponse{
 
 async fn gpt(args: &Vec<String>) -> Result<(), Box<dyn std::error::Error>> {
   
-  let hugging_face_key = env::var("HUGGINGFACE_ACCESS_TOKEN").expect("HuggingFace Key Not Set");
-  let gpt_model = env::var("GPT_MODEL").expect("GPT Module Not Set");
+  let hugging_face_key = env::var("KEY").expect("HuggingFace Key Not Set");
+  let gpt_model = env::var("MODEL").expect("GPT Module Not Set");
 
   let mut payload = String::new();
   
@@ -20,7 +19,7 @@ async fn gpt(args: &Vec<String>) -> Result<(), Box<dyn std::error::Error>> {
       payload += &(" ".to_owned() + &args[count]);
   }
   
-  println!("{}",payload);
+  //println!("{}",payload);
   let header = "Bearer ".to_owned() + &hugging_face_key;
   let client = reqwest::Client::new();
   let response = client
@@ -33,11 +32,10 @@ async fn gpt(args: &Vec<String>) -> Result<(), Box<dyn std::error::Error>> {
   
   let hugging_face_response: HuggingFaceResponse = response.json().await?;
   
-  println!("\n Generated Text: {}", hugging_face_response.generated_text);
+  println!("\n Model: {}", hugging_face_response.generated_text);
 
   Ok(())
 }
-
 
 pub async fn start(){
   dotenv().ok();
@@ -46,55 +44,55 @@ pub async fn start(){
 
 async fn fillter_args(){
   let args:Vec<String> = env::args().collect();
+
   if chk_agrs(&args) == -1 || args.len() < 3{
     println!("Arrugment Formation Is Worng... Use -h For Help");
     return;
   }
-  for arg in &args{
-    //print!("\n Arrgument : {} ",arg);
+    //print!("\n Arrgument : {} ",args[2]);
 
-    if arg == "-h" || arg == "--help" {
+    if args[2] == "-h" || args[2] == "--help" {
       println!("AssistX CLI Tool \n");
       println!("Usage: AssistX [OPTIONS] [PAYLOAD|INPUT]\n");
       println!("Example: AssistX --img-txt -p 'Man Standing One The Moon'\n");
       println!("Options:");
       println!("  -h,--help        Help");
-      println!("  -p,--payload     Used To Denote Input");
       println!("  -?,--gpt         Ask GPT Model");
-      println!("  --txt-img        Text To Image");
-      println!("  --img-class      Image Classification");
-      println!("  --img-txt        Image To Text\n\n");
-      println!("  --text-voice     Text To Voice\n\n");
-
+      println!("  --version        Check Version");
+      println!("  --update         Check For Update");
+      println!("  --setup          Setup Assistx");
+      println!("  --uninstall      Uninstall The Assist-Tool");
+      println!("  --about          About The Assistx-Tool");
+      
       return;
     }
-    if arg == "-?" || arg == "--gpt"{
-      //print!("Entered GPT");
-      let _ = gpt(&args).await;
-    }
-    else if arg == "--txt-img"{
-      txt_img(&args);
-    }
-    else if arg == "--img-txt"{
-      img_txt(&args)
-    }
-    else if arg == "--img-class"{
-      img_class(&args);
-    }else if arg == "--version"{
-      println!("\n AssistX CLI Version 1.0")
-    }else if arg == "--update"{
-      println!("Checking For Updates...")
-    }else if arg == "--setup"{
-      println!("");
-    }else if arg == "--uninstall"{
-      println!("Uninstalling The Assistx...");
-    }else if arg == "--about"{
-      println!("\n AssistX CLI Version 1.0");
-      println!("Assistx Tool Helps To Connect To LLM Via Throught CLI , Makes Work Fast And Easy And Streamless ");
+    if args[2] == "-h" || args[2] == "--help" {
+      println!("AssistX CLI Tool \n");
+      println!("Usage: AssistX [OPTIONS] [PAYLOAD|INPUT]\n");
+      println!("Example: AssistX --img-txt -p 'Man Standing One The Moon'\n");
+      println!("Options:");
+      println!("  -h,--help        Help");
+      println!("  -?,--gpt         Ask GPT Model");
+    
+      return;
     }
 
-  }
-}
+    if args[2] == "-?" || args[2] == "--gpt"{
+      let _ = gpt(&args).await;
+    }
+  
+    else if args[2] == "--version"{println!("\n AssistX CLI Version 1.0");}
+    else if args[2] == "--update"{println!("\n No Update Available");}
+    else if args[2] == "--setup"{
+      println!("\n Setup : You Can Find A .env File In The Folder You Have Installed The CLI-Tool Open It , Then Past The API Key In The KEY field and Model api in Model \n Field Then Save And Exit , Know Add The Assist.exe or assistx.rs With The Full Path Of The File \n To The Enviroment Variable You Can Find In Youtube How To Add Enviroment Variable , After That You Can Open \n The cmd And Excute Assistx --version to check if it works");
+    }
+    else if args[2] == "--uninstall"{println!("\n Uninstall The Eniter Folder Assistx-CLI From The Folder You Installed It \n Note: You Need To Remove The assist.exe or assist.rs From The Enviroment Variables To Uninstall It Fully...");}
+    else if args[2] == "--about"{
+      println!("\n AssistX CLI Version 1.0");
+      println!("\n Assistx Tool Helps To Connect To LLM Via Throught CLI , Makes Work Fast And Easy And Streamless ");
+    }
+
+    }
 
 fn chk_agrs(args: &Vec<String>) -> i32 {
 
@@ -103,36 +101,9 @@ fn chk_agrs(args: &Vec<String>) -> i32 {
   let regex = Regex::new(&options_patter_combined).unwrap();
   let options_test = regex.is_match(&args[2]);
 
-  //println!("Args[2] : {}", args[2]);
-  //println!("{}", options_test);
 
-  if options_test == false{ // if the regex don't match, then the values of options_test would be false 
+  if options_test == false{ 
     return -1;
   }
   return 0;
 }
-
-
-async fn txt_img(args: &Vec<String>) -> Result<(), Box<dyn std::error::Error>>{
-  let api_url = "https://api-inference.huggingface.co/models/microsoft/DialoGPT-medium";
-  
-  let payload = &args[2];
-
-  let client = reqwest::Client::new();
-  let response = client
-      .post(api_url)
-      .header("Authorization", "Bearer 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")
-      .body(payload.to_owned())
-      .send()
-      .await?; 
-
-  
-  let hugging_face_response: HuggingFaceResponse = response.json().await?;
-  
-  println!("\n Generated Text: {}", hugging_face_response.generated_text);
-
-  Ok(())
-
-}
-fn img_txt(args: &Vec<String>){}
-fn img_class(args: &Vec<String>){}
